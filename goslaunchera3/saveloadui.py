ddba7586ddd800d6f94f013ddeb09607269ce892
ToF <Christophe.taudin@gmail.com>
2017-02-23 15:36:11 +0100

    Ajoute variable excludeWidgetList du module saveloadui.py

--- goslaunchera3/saveloadui.py
+++ goslaunchera3/saveloadui.py
@@ -10,7 +10,7 @@ from PyQt5.QtWidgets import  *
 import inspect
 from distutils.util import strtobool
 
-
+excludeWidgetList=["comboBox_ChoixProfil"]
 #===================================================================
 # save "ui" controls and values to registry "setting"
 # currently only handles comboboxes editlines & checkboxes
@@ -23,34 +23,35 @@ def guisave(ui, settings):
     #for child in ui.children():  # works like getmembers, but because it traverses the hierarachy, you would have to call guisave recursively to traverse down the tree
 
     for name, obj in inspect.getmembers(ui):
-      # if type(obj) is QComboBox:  # this works similar to isinstance, but missed some field... not sure why?
-        if isinstance(obj, QComboBox):
+  
+        # if type(obj) is QComboBox:  # this works similar to isinstance, but missed some field... not sure why?
+        if isinstance(obj, QComboBox) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()  # get combobox name
             index = obj.currentIndex()  # get current index from combobox
             text = obj.itemText(index)  # get the text for current index
             settings.setValue(name, text)  # save combobox selection to registry
 
-        if isinstance(obj, QLineEdit):
+        if isinstance(obj, QLineEdit) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = obj.text()
             settings.setValue(name, value)  # save ui values, so they can be restored next time
 
-        if isinstance(obj, QCheckBox):
+        if isinstance(obj, QCheckBox) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             state = obj.isChecked()
             settings.setValue(name, state)
 
-        if isinstance(obj, QRadioButton):
+        if isinstance(obj, QRadioButton) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = obj.isChecked()  # get stored value from registry
             settings.setValue(name, value)
             
-        if isinstance(obj, QSpinBox):
+        if isinstance(obj, QSpinBox) and obj.objectName() not in excludeWidgetList:
             name  = obj.objectName()
             value = obj.value()             # get stored value from registry
             settings.setValue(name, value)
 
-        if isinstance(obj, QSlider):
+        if isinstance(obj, QSlider) and obj.objectName() not in excludeWidgetList:
             name  = obj.objectName()
             value = obj.value()             # get stored value from registry
             settings.setValue(name, value)    
@@ -65,7 +66,7 @@ def guisave(ui, settings):
 def guirestore(ui, settings):
 
     for name, obj in inspect.getmembers(ui):
-        if isinstance(obj, QComboBox):
+        if isinstance(obj, QComboBox) and obj.objectName() not in excludeWidgetList:
             index = obj.currentIndex()  # get current region from combobox
             # text   = obj.itemText(index)   # get the text for new selected index
             name = obj.objectName()
@@ -84,30 +85,30 @@ def guirestore(ui, settings):
             else:
                 obj.setCurrentIndex(index)  # preselect a combobox value by index
 
-        if isinstance(obj, QLineEdit):
+        if isinstance(obj, QLineEdit) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = (settings.value(name))  # get stored value from registry
             obj.setText(value)  # restore lineEditFile
 
-        if isinstance(obj, QCheckBox):
+        if isinstance(obj, QCheckBox) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = settings.value(name)  # get stored value from registry
             if value != None:
                 obj.setChecked(strtobool(value))  # restore checkbox
 
-        if isinstance(obj, QRadioButton):
+        if isinstance(obj, QRadioButton) and obj.objectName() not in excludeWidgetList:
            name = obj.objectName()
            value = settings.value(name)  # get stored value from registry
            if value != None:
                obj.setChecked(strtobool(value))
 
-        if isinstance(obj, QSlider):
+        if isinstance(obj, QSlider) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = settings.value(name)    # get stored value from registry
             if value != None:           
                 obj. setValue(int(value))   # restore value from registry
 
-        if isinstance(obj, QSpinBox):
+        if isinstance(obj, QSpinBox) and obj.objectName() not in excludeWidgetList:
             name = obj.objectName()
             value = settings.value(name)    # get stored value from registry
             if value != None:
